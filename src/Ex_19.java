996cee0b1765896b9b26efacb07c86477b664e4d
José Froes <josepaulo.froes@gmail.com>
2025-11-21 18:25:48 -0300

    Exercicio 19: Combine Predicate + Function para processar e filtrar lista.

---
+++ src/Ex_19.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class Ex_19 {
+//    Exercicio 19: Combine Predicate + Function para processar e filtrar lista.
+
+
+
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+
+        Predicate<Integer> even = n -> n % 2 == 0;
+        Predicate<Integer> odd = Predicate.not(even);
+
+        Function<Integer, Integer> plusTwo = n -> n + 2;
+
+        numeros.stream().filter(odd).map(plusTwo).toList().forEach(System.out::println);
+
+
+
+    }
+}
